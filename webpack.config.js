833dff38770100bc282c1001fe4b47cd64872bc4
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-12 01:24:34 +0200

    Modify dependencies and building

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = function (env) {
 
@@ -19,11 +20,17 @@ module.exports = function (env) {
             extensions: ['.ts', '.tsx', '.js', '.jsx']
         },
         plugins: [
-            new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' })
+            new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+            new ExtractTextPlugin('style.css')
         ],
         module: {
             rules: [
-                { test: /\.css?$/, use: ['style-loader', 'css-loader'] }
+                { test: /\.css?$/, use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                  }) },
+                { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' },
+                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' }
             ]
         }
     }
--- wwwroot/src/main.js
+++ wwwroot/src/main.js
@@ -1,5 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'ionicons/css/ionicons.min.css'
+import 'admin-lte/dist/css/AdminLTE.min.css';
+import 'admin-lte/dist/css/skins/skin-blue.min.css';
+
+import 'bootstrap'
 import 'jquery';
-import 'bootstrap';
+
 import '../js/site.js';
-import '../css/site.css';
