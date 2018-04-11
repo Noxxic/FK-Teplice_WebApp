59c318aab1ad8d4fd8cc7e0ec2e1540e8e65e740
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-12 01:50:44 +0200

    Prepare global layout

--- wwwroot/src/main.js
+++ wwwroot/src/main.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'ionicons/css/ionicons.min.css'
+import 'font-awesome/css/font-awesome.min.css';
+import 'ionicons/css/ionicons.min.css';
 import 'admin-lte/dist/css/AdminLTE.min.css';
 import 'admin-lte/dist/css/skins/skin-blue.min.css';
 
-import 'bootstrap'
+import 'bootstrap';
 import 'jquery';
+import 'admin-lte';
 
 import '../js/site.js';
