0fc63bf4e043f13b1bc748c981f940b02a7d8de7
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-13 14:34:49 +0200

    Modify top menu routing

--- ViewComponents/TopMenu.cs
+++ ViewComponents/TopMenu.cs
@@ -3,12 +3,21 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace FKTeplice.ViewComponents {
     public class TopMenu : ViewComponent {
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var menuItems = new List<Tuple<string, string, string>>() {
+                Tuple.Create("Domů", "Home", "Index"),
+                Tuple.Create("Informace", "Home", "Index"),
+                Tuple.Create("Fitness", "Home", "Index"),
+                Tuple.Create("Medical", "Home", "Index"),
+                Tuple.Create("Trenér", "Home", "Index"),
+            };
+
+            return View(menuItems);
         }
     }
 }
