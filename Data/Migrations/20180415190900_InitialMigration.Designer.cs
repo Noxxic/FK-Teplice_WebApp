2428149511c0e32687164e7fe58100153069953d
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-15 21:09:26 +0200

    Add initial migration

--- Data/Migrations/20180415190900_InitialMigration.Designer.cs
+++ Data/Migrations/20180415190900_InitialMigration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FKTeplice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180415190900_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
