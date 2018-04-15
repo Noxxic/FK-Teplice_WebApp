e5cd23d6a1e8e99d61fd5278bdf902079aa4f9e5
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-15 23:14:48 +0200

    Make player team nullable

--- Data/Migrations/20180415211219_MakePlayerTeamNullable.Designer.cs
+++ Data/Migrations/20180415211219_MakePlayerTeamNullable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FKTeplice.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180415211219_MakePlayerTeamNullable")]
+    partial class MakePlayerTeamNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20180415211219_MakePlayerTeamNullable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FKTeplice.Data.Migrations
+{
+    public partial class MakePlayerTeamNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TeamId",
+                table: "Players",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Teams_TeamId",
+                table: "Players",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
