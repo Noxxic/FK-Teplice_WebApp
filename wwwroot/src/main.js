58a82d04daa101c743c98ff42b6f0e5b6b4f6614
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-18 19:41:13 +0200

    Add datatables to project

--- wwwroot/src/main.js
+++ wwwroot/src/main.js
@@ -1,14 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'select2/dist/css/select2.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'ionicons/css/ionicons.min.css';
 import 'admin-lte/dist/css/AdminLTE.min.css';
 import 'admin-lte/dist/css/skins/skin-blue.min.css';
-import 'select2/dist/css/select2.css'
-import '../css/site.css'
+import 'datatables.net-bs/css/dataTables.bootstrap.css';
+
+import '../css/site.css';
 
 import 'bootstrap';
 import 'jquery';
 import 'admin-lte';
-import 'select2/dist/js/select2.js'
+import 'datatables/media/js/jquery.dataTables.js';
+import 'select2/dist/js/select2.js';
+import 'datatables.net-bs/js/dataTables.bootstrap.js';
+
 
 import '../js/site.js';
