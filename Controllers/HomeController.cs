889ec646785a4ef84a917eecc9b43a3f81b87b11
Lukáš <lukaszahradnik96@seznam.cz>
2018-04-21 22:46:22 +0200

    Add basic index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,14 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FKTeplice.Models;
+using FKTeplice.Models.HomeViewModels;
+using FKTeplice.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FKTeplice.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext _context) {
+            this._context = _context;
+        }
+
+        [AllowAnonymous]
         public IActionResult Index()
         {
-            return View();
+            var model = new IndexHomeModel();
+            model.TeamsCount = _context.Teams.Count();
+            model.PlayersCount = _context.Players.Count();
+            return View(model);
         }
 
         public IActionResult About()
